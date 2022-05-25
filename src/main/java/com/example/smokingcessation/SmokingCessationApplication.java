3198ef427c8c066ca8385b541f6b3a29693964d2
Jeman Mama <jeman.mama@orangeandbronze.com>
2022-05-25 15:30:31 +0800

    wazap

--- src/main/java/com/example/smokingcessation/SmokingCessationApplication.java
+++ src/main/java/com/example/smokingcessation/SmokingCessationApplication.java
@@ -27,7 +27,7 @@ public class SmokingCessationApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://smokingcessation.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://smokingcessation.herokuapp.com", "http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
 				"Accept","Authorization","Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method","Access-Control-Request-Headers"));
