f6fbc59cf104dabb0f809d3f87d2b0b6c58ac789
Jeman Mama <jeman.mama@orangeandbronze.com>
2022-05-25 16:06:30 +0800

    fixed cors error

--- src/main/java/com/example/smokingcessation/config/BadgeConfig.java
+++ src/main/java/com/example/smokingcessation/config/BadgeConfig.java
@@ -16,29 +16,16 @@ public class BadgeConfig {
     CommandLineRunner commandLineRunnerBadge(BadgeRepository repository){
         return args -> {
             ArrayList<Badge> badges = new ArrayList<>();
-            badges.add(new Badge("New Start", "Created an account in UPOS",0,Integer.MAX_VALUE));
-            badges.add(new Badge("1 Day Free", "Have not smoked for 1 day",1,Integer.MAX_VALUE));
-            badges.add(new Badge("3 Days Free", "Have not smoked for 3 days",3,Integer.MAX_VALUE));
-            badges.add(new Badge("1 Week Free", "Have not smoked for 1 week",7,Integer.MAX_VALUE));
-            badges.add(new Badge("2 Weeks Free", "Have not smoked for 2 weeks",14,Integer.MAX_VALUE));
-            badges.add(new Badge("Richer by 100", "Saved a total of PHP 100.00",Integer.MAX_VALUE,100));
-            badges.add(new Badge("Richer by 500", "Saved a total of PHP 500.00",Integer.MAX_VALUE,500));
-            badges.add(new Badge("Richer by 1K", "Saved a total of PHP 1000.00",Integer.MAX_VALUE,1000));
-            badges.add(new Badge("Richer by 2K", "Saved a total of PHP 2000.00",Integer.MAX_VALUE,2000));
+            badges.add(new Badge("New Start|Bagong Umpisa", "Created an account in UPOS|Nakagawa ng account sa UPOS",0,Integer.MAX_VALUE));
+            badges.add(new Badge("1 Day Free|Isang Araw Walang Yosi", "Have not smoked for 1 day|Isang araw ng hindi nakapagyosi",1,Integer.MAX_VALUE));
+            badges.add(new Badge("3 Days Free|Tatlong Araw Walang Yosi", "Have not smoked for 3 days|Tatlong araw ng hindi nakapagyosi",3,Integer.MAX_VALUE));
+            badges.add(new Badge("1 Week Free|Isang Linggo Walang Yosi", "Have not smoked for 1 week|Isang linggo ng hindi nakapagyosi",7,Integer.MAX_VALUE));
+            badges.add(new Badge("2 Weeks Free|Dalawang Linggo Walang Yosi", "2 Weeks Free|Dalawang Linggo Walang Yosi",14,Integer.MAX_VALUE));
+            badges.add(new Badge("Richer by 100|Yumaman ng 100", "Saved a total of PHP 100.00|Nakapagtipid ng 100.00",Integer.MAX_VALUE,100));
+            badges.add(new Badge("Richer by 500|Yumaman ng 500", "Saved a total of PHP 500.00|Nakapagtipid ng 500.00",Integer.MAX_VALUE,500));
+            badges.add(new Badge("Richer by 1K|Yumaman ng 1000", "Saved a total of PHP 1000.00|Nakapagtipid ng 1000.00",Integer.MAX_VALUE,1000));
+            badges.add(new Badge("Richer by 2K|Yumaman ng 2000", "Saved a total of PHP 2000.00|Nakapagtipid ng 2000.00",Integer.MAX_VALUE,2000));
             repository.saveAll(badges);
         };
     }
 }
-
-
-/*
-{name: "1 Day Free", description: "Have not smoked for 1 day", obtained: true},
-      {name: "3 Days Free", description: "Have not smoked for 3 days", obtained: true},
-      {name: , obtained: false},
-      {name: , obtained: false},
-      {name: , obtained: true},
-      {name: , obtained: true},
-      {name: , obtained: false},
-      {name: , obtained: false}
-
- */
