00b3f38edb4f6736964f4452bc6594d8602fd99c
Jeman Mama <jeman.mama@orangeandbronze.com>
2022-06-02 18:04:33 +0800

    asdasd

--- src/main/java/com/example/smokingcessation/config/PostConfig.java
+++ src/main/java/com/example/smokingcessation/config/PostConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+//@Configuration
 public class PostConfig {
-    @Bean
+//    @Bean
     CommandLineRunner commandLineRunnerPost(PostRepository postRepository, CommentRepository commentRepository, UserRepository userRepository){
         return args -> {
 
